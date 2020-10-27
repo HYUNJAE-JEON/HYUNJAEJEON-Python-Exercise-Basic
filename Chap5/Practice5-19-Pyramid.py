40b18ab857bc3b85d9c20b4066ac6919ad36e172
HYUNJAEJEON <ysysyshy@gmail.com>
2020-10-27 19:55:29 +0900

    Python exercise - 20201027 - good

--- Chap5/Practice5-19-Pyramid.py
+++ Chap5/Practice5-19-Pyramid.py
@@ -1,42 +1,36 @@
-countofrows = eval(input("행의 개수를 입력해주세요: "))
+# countofrows = eval(input("행의 개수를 입력해주세요: "))
 #
-# for i in range(countofrows):
-#     for j in range(countofrows // 2):
-#         print(" ")
-#
-#
-#
-# print(format(59832, "10d"))
-# print(format(59832, "<10d"))
-#
-# for i in range(0,2*countofrows,-2):
-#     2*countofrows
-#     print("{0^")
-
-# for x in range(1, countofrows+1):
-#     print(end="")
-#     for y in range(1, x+1):
-#         y = str(y)
-#         print(y.ljust(countofrows+1))
-
-# for i in range(1, countofrows+1):
-#     for j in range(1,i):
-#         print(' '*(countofrows-i), print(j))
-#
-for i in range(1,countofrows+1):
-    x = countofrows
-    print(end=" ")
-    for j in range(1,i):
-        print(str(j), end="")
-    print()
-
-
-# n = int(input())
-# for i in range(1, n+1):
-#     for j in range(0, i):
-#         print('*', end='')
+# for i in range(1,countofrows+1):
+#     for j in range(countofrows-i,0,-1):
+#         print("    "),
+#     for j in range(i,0,-1):
+#         print(format(j, "4d")),
+#     for j in range(2, i+1):
+#         print(format(j,"4d")),
 #     print()
-# for i in range(n-1, 0, -1):
-#     for j in range(0, i):
-#         print('*', end='')
-#     print()
+#
+#
+num = eval(input("1부터 15까지의 숫자를 입력해주세요: "))
+
+
+space = " "
+
+if num <= 15:
+
+    for i in range(1, num+1):
+        for num_of_spaces in range(i+1, 1, -num):
+            x = (i-1)
+            if i <= 9:
+                spaces = space*(num-x+7)
+            if (i >9) and (i <=15):
+                spaces = space * (num - x-(i%9)+7)
+            print(spaces, end="")
+        for inv_rec in range(i, 1, -1):
+            print(inv_rec,end="")
+        for rec in range(1, i+1):
+            print(rec, end="")
+        print("")
+
+
+else:
+    print("15 이하의 숫자를 입력해주세요!")
